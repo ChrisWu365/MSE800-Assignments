a40566b29a02f70a11aba676eda45fad9fb14b4b
ChrisWu365 <wukangrong@hotmail.com>
2025-04-13 11:00:28 +1200

    Activity Week1 - using NumPy Tasks: Print the average temperature for a week. Find the highest and lowest temperature recorded. Convert all temperatures to Fahrenheit and print the result. (Formula: F = C * 9/5 + 32) Identify the days (by index) where the temperature was above 20°C.
    Help syntax:
    
    import numpy as np
    
    temperatures = np.array([18.5, 19, 20, 25.0, 2, 30, 13.9])

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ import numpy as np
 
 def calctemperature():
     # Use a breakpoint in the code line below to debug your script.
-    data = np.array([17,18,19,20,21,22,23])
+    data = np.array([18.5, 19, 20, 25.0, 2, 30, 13.9])
     avg = np.average(data)
     highest = np.amax(data)
     lowest = np.amin(data)
     fahrenheit = data * 9 / 5 + 32
     indices = np.where(data > 20)
-    print("The average temperature for the week: ", avg)
-    print("The highest temperature for the week: ", highest)
-    print("The lowest temperature for the week: ", lowest)
-    print("The Fahrenheit for the week: ", fahrenheit)
-    print("The days (by index) where the temperature was above 20°C: ", indices)
+    print("The temperature for the week: ", data)
+
+    print(f"1. The average temperature for the week: {avg:.1f}")
+
+    print("2. The highest temperature: %s and the lowest temperature: %s" % (highest, lowest))
+
+    print("3. The Fahrenheit for the week: ", fahrenheit)
+
+    # the index starts from 0 which means the first day of the given week
+    print("4. The days (by index) where the temperature was above 20°C: ", indices)
 
 
 # Press the green button in the gutter to run the script.

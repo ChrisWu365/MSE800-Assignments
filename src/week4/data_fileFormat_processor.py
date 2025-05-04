5316d02775526988168caad8722d7251b9f13661
wukangrong <wukangrong@iqdnet.cn>
2025-05-04 22:18:46 +1200

    1. Create a Full project in Python - object oriented - File processing 2. Using union and symmetric difference and Math and circle Libs

--- src/week4/data_fileFormat_processor.py
+++ src/week4/data_fileFormat_processor.py
@@ -1,6 +1,18 @@
+'''
+Activity W4-2-3: - Create a Full project in Python - object oriented - File processing
+Note: You must have main function
+Write a full project to do the data file processing for csv, text, image and etc. file formats.
+
+Last activity for Week 4: Using union and symmetric difference and Math and circle Libs
+Update the data format file to be added two more functionalities:
+1- call Math function and get value from end user to calculate Sin and Cos
+2- get diameter of a circle and calculate area of the circle.
+3- Try to test the union and symmetric difference methods
+'''
 import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import math
 
 class DataFileFormat_Processor:
     def __init__(self, file_path):
@@ -58,16 +70,49 @@ class DataFileFormat_Processor:
         else:
             print(self.data)
 
+    def calc_sin_cos_from_input(self):
+        # 1- call Math function and get value from end user to calculate Sin and Cos
+        n = float(input('Enter a number to calculate its sine and cosine: '))
+        print(f'The sine of [ {n} ] is {math.sin(n)}')
+        print(f'The cosine of [ {n} ] is {math.cos(n)}')
+
+    def calc_circle_diameter_area(self):
+        # 2- get diameter of a circle and calculate area of the circle with given radius
+        radius = float(input('Enter a radius to calculate the diameter and area of the circle: '))
+        diameter = 2 * radius
+        area = math.pi * radius ** 2
+        print(f'Given radius [ {radius} ], the diameter is: [ {diameter} ] and the area of the circle is [ {area} ]')
+
+    def calc_union(self, set_a, set_b):
+        # 3- Try to test the union and symmetric difference methods
+        set_c = set_a.union(set_b)
+        print(f'The union between {set_a} and {set_b} is: [{set_c}]')
+        return set_c
+
+    def calc_symmetric_difference(self, set_a, set_b):
+        # 3- Try to test the union and symmetric difference methods
+        set_c = set_a.symmetric_difference(set_b)
+        print(f'The symmetric difference between {set_a} and {set_b} is: [{set_c}]')
+        return set_c
+    
 def main():
     
     # file_path = 'src/Week4/sample_junk_mail.csv'
-    # file_path = 'src/Week4/sample_text.txt' 
+    file_path = 'src/Week4/sample_text.txt' 
     # file_path = 'src/Week4/Sample_data_2.parquet' 
-    file_path = 'cifar10'
+    # file_path = 'cifar10'
     processor = DataFileFormat_Processor(file_path)
     processor.load_data()
     processor.initial_processing()
     processor.process_data()
+
+    processor.calc_sin_cos_from_input()
+    processor.calc_circle_diameter_area()
+
+    set_a = set([2, 3, 4])
+    set_b = set([3, 4, 5])
+    processor.calc_union(set_a, set_b)
+    processor.calc_symmetric_difference(set_a, set_b)
 
 if __name__ == "__main__":
     main()

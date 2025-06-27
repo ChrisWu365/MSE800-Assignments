c520c89d3eef3319ac610fee84f86885696ad950
ChrisWu365 <wukangrong@hotmail.com>
2025-06-28 11:36:13 +1200

    add variable path in the route

--- src/week12/helloworld_flask.py
+++ src/week12/helloworld_flask.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/username/<username>")
+def learn(username):
+    return f"{username} is learning Flask! That's amazing!!!"
+
 if __name__ == "__main__":
     app.run(debug=True)
